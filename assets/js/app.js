921ea6475dc77c033a201049860530fee4ee792b
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2025-04-03 00:54:34 +0200

    Site AFMAT

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+const threshold = .1
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold
+}
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > threshold) {
+      entry.target.classList.remove('reveal')
+      observer.unobserve(entry.target)
+    }
+  })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+
+window.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver(handleIntersect, options)
+  const targets = document.querySelectorAll('.reveal')
+  targets.forEach(function (target) {
+    observer.observe(target)
+  })
+})
+
+// METTRE A JOUR AUTOMATIQUEMENT L'ANNEE DANS LE FOOTER 
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
